aacdce846f32e3dbcc9b76939d2b04a9bed28d0d
Arthur Inzhilov <artur-inzhilov@mail.ru>
2023-05-30 12:56:09 +0500

    Техническое задание проекта №14 <style> fixed

--- src/test/java/ru/practicum/shareit/item/CommentRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/CommentRepositoryTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import ru.practicum.shareit.item.model.CommentEntity;
 import ru.practicum.shareit.item.model.ItemEntity;
 import ru.practicum.shareit.item.repository.CommentRepository;
-import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.UserEntity;
 
 import java.util.List;
